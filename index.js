66401766153fca52b911566d09f034d42745ad1e
Akin Mert Bur <akinmertbur@gmail.com>
2024-04-16 09:44:56 +0300

    create qr_url txt file to save the user input

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ inquirer
     },
   ])
   .then((answers) => {
+    // Get input from the user-prompt
     const answerUrl = answers.URL;
 
-    var qr_svg = qr.image(answerUrl);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    // Create qr-image
+    var qr_img = qr.image(answerUrl);
+    qr_img.pipe(fs.createWriteStream("qr_img.png"));
+
+    // Create qr_url txt file to save the user input
+    fs.writeFile("qr_url.txt", answerUrl, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
